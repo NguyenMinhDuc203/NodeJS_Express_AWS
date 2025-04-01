888f933e860ca63158c81a7a0e87647ca829bbb9
NguyenMinhDuc203 <minhduc5068@gmail.com>
2025-04-01 07:01:45 +0700

    connect express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // npm install ejs, nodemon, express, multer, aws-sdk
+
+const express = require("express");
+const multer = require("multer");
+
+
+const app = express();
+app.use(express.static("./views"));
+app.set("views", "./view");
+app.set("view engine", "ejs");
+
+
+app.listen(3000, ()=>{
+    console.log("Running on port 3000...")
+});
+
+app.get("/",(req, res)=>{
+    return res.render("index");
+});
+
