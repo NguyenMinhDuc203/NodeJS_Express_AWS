6c2c3df3de8eec1e337062522f3daa0559cfa2c1
NguyenMinhDuc203 <minhduc5068@gmail.com>
2025-04-02 11:48:27 +0700

    CRD with express, localStorage

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const express = require("express");
 const multer = require("multer");
 
-
+const data = require("./data");
+const upload = multer();
 const app = express();
-app.use(express.static("./views"));
+app.use(express.static("./view"));
 app.set("views", "./view");
 app.set("view engine", "ejs");
 
@@ -15,6 +16,27 @@ app.listen(3000, ()=>{
 });
 
 app.get("/",(req, res)=>{
-    return res.render("index");
+    return res.render("index", {data:data});
 });
 
+app.post("/", upload.none(),(req, res)=>{
+
+    data.push(req.body);
+    console.log(req.body);
+    return res.redirect("/");
+});
+
+app.post("/del/:cccd", (req, res)=>{
+
+    const {cccd} = req.params;
+    const index = data.findIndex(item => item.cccd===cccd);
+    if(index !== -1){
+        data.splice(index, 1);
+        console.log("Xoa thanh cong.");
+    }
+    else{
+
+        console.error(`Loi!!! Xoa khong thanh cong.`);
+    }
+    return res.redirect("/");
+});
